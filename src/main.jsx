f64cd4604445dd2f9f9466228374772f7c28bed7
SAHILKUMARNEHRA <sk.nehra2005@gmail.com>
2026-05-20 11:15:18 +0530

    fix: responsive layout + console warnings

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <UserProvider>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </UserProvider>,
